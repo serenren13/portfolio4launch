305c969963a73861bc2f269724faf334166ffb26
Serenity Phillips <sphillips@unc.edu>
2026-05-18 16:28:58 -0400

    added About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,11 @@
 export default function About() {
-    return <h1>About Page</h1>
+    return <div>
+
+        <h1>Hi, I'm Serenity Phillips</h1>
+        <p>I attend UNC, I enjoy coding</p>
+        <h2>Here are some coding languages I enjoy...</h2>
+        <ul>
+            JSX, JS, React, HTML, CSS, Python, WebGL
+        </ul>
+    </div>
 }
